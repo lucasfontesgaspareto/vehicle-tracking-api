baf89f173dff99446a03fe3e414a49f85f4fa880
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2024-12-11 12:49:25 -0300

    chore: working on prisma module

--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RoutesService {
+  constructor(private readonly prismaClient: PrismaService) {}
+
   create(createRouteDto: CreateRouteDto) {
     return 'This action adds a new route';
   }
